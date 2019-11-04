8b92b60023ac6490a312fe6d9479f13c68a371c7
JFSolution <muskel_schlumpfi@yahoo.de>
2019-11-04 22:04:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 plugin_identifier = "Octoslack_JFS"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_Octoslack_JFS"
+plugin_package = "octoprint_Octoslack"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
